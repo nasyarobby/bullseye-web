bb17b49878c982d1f0ddb3ab9abcd04d2a90f3ad
Nasyarobby (mbpro-tik) <nasyarobby@gmail.com>
2024-03-15 13:39:42 +0700

    add Icons

---
+++ src/components/Icons.tsx
@@ -0,0 +1,13 @@
+import { 
+    FaExclamationCircle as RedisDisconnectedIcon,
+    FaPencilAlt as EditIcon,
+    FaPlug as NewConnectionIcon,
+    FaTrash as DeleteIcon } from "react-icons/fa";
+
+
+export {
+    NewConnectionIcon,
+    EditIcon,
+    DeleteIcon,
+    RedisDisconnectedIcon
+}
